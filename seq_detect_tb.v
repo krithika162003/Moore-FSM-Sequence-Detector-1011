45aaa71cc382b2ca5374ca8c19c1a25a0279b105
Krithika <krithikabalu2003@gmail.com>
2025-10-20 20:37:56 +0530

    Create seq_detect_tb.v

---
+++ seq_detect_tb.v
@@ -0,0 +1,36 @@
+module moore_testbench; 
+ 
+    // Inputs 
+    reg x; 
+    reg clk; 
+    reg reset; 
+    // Outputs 
+    wire z; 
+ 
+    // Instantiate the Unit Under Test (UUT) 
+    Moore_sequence uut ( 
+        .x(x), 
+        .clk(clk), 
+        .reset(reset), 
+        .z(z) 
+    ); 
+    
+initial 
+    begin 
+        clk = 1'b0; 
+        reset = 1'b1; 
+        #15 reset = 1'b0; 
+    end 
+ 
+always #5 clk = ~ clk;   
+ 
+initial begin 
+        #12 x = 0;#10 x = 0 ; #10 x = 1 ; #10 x = 0 ; 
+        #12 x = 1;#10 x = 1 ; #10 x = 0 ; #10 x = 1 ; 
+ 
+ 
+        #12 x = 1;#10 x = 0 ; #10 x = 0 ; #10 x = 1 ; 
+        #12 x = 0;#10 x = 1 ; #10 x = 1 ; #10 x = 0 ; 
+      
+    end 
+endmodule
